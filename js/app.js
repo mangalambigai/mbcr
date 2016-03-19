86df2b7854bee0eee0e19e62c488d4a8046629d1
mangalambigai <mangalambigais@gmail.com>
2016-03-19 18:18:25 -0400

    JSbeautify, Jshint and log the db entries filtered on stop and time

--- js/app.js
+++ js/app.js
@@ -6,31 +6,31 @@ angular.module('mbta', [])
     var scheduleList = this;
 
     $scope.init = function() {
-        $scope.schedules=[];
+        $scope.schedules = [];
         $scope.maxHours = 2;
     };
 
     $scope.getSchedule = function() {
         if ($scope.depStation && $scope.destStation)
             $scope.getTripsByStation($scope.depStation, $scope.destStation, $scope.maxHours);
-    }
+    };
 
     $scope.getTripsByStation = function(depStation, destStation, maxHours) {
         fetch(
             'http://realtime.mbta.com/developer/api/v2/schedulebystop?api_key=xGeHtAQ3kk2mYyhD4fO8rw&stop=' +
-            depStation + '&max_time=' + maxHours*60, {
+            depStation + '&max_time=' + maxHours * 60, {
                 method: 'GET'
-            }).then(function (response) {
+            }).then(function(response) {
             return response.json();
-        }).catch(function (error) {
+        }).catch(function(error) {
             console.log(error);
-        }).then(function (trips) {
+        }).then(function(trips) {
 
-            var tripids=[];
-            trips.mode.forEach(function (mode) {
-                mode.route.forEach(function (route) {
-                    route.direction.forEach(function (direction) {
-                        direction.trip.forEach(function (trip) {
+            var tripids = [];
+            trips.mode.forEach(function(mode) {
+                mode.route.forEach(function(route) {
+                    route.direction.forEach(function(direction) {
+                        direction.trip.forEach(function(trip) {
                             tripids.push(trip.trip_id);
                         });
                     });
@@ -38,30 +38,30 @@ angular.module('mbta', [])
             });
 
             Promise.all(tripids.map($scope.getScheduleByTrip))
-            .then(function(response){
-                $scope.displaySchedules(response, depStation, destStation, maxHours);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+                .then(function(response) {
+                    $scope.displaySchedules(response, depStation, destStation, maxHours);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
         });
-    }
+    };
 
-    $scope.getScheduleByTrip = function (trip) {
+    $scope.getScheduleByTrip = function(trip) {
         return fetch('http://realtime.mbta.com/developer/api/v2/schedulebytrip?' +
             'api_key=xGeHtAQ3kk2mYyhD4fO8rw&trip=' + trip, {
                 method: 'GET'
-        }).then(function (response) {
+            }).then(function(response) {
             return response.json();
         });
     };
 
     $scope.displaySchedules = function(schedules, depStation, destStation, maxHours) {
-        schedules.forEach(function (schedule) {
+        schedules.forEach(function(schedule) {
             var foundStart = false,
                 foundStop = false;
-            var stops=[];
-            angular.forEach(schedule.stop, function (stop) {
+            var stops = [];
+            angular.forEach(schedule.stop, function(stop) {
                 if (stop.stop_name === depStation)
                     foundStart = true;
 
@@ -79,20 +79,23 @@ angular.module('mbta', [])
                     stops.push({
                         stop_name: stop.stop_name,
                         sch_arr_dt: arrTime.toLocaleTimeString(),
-                        sch_dep_dt: depTime.toLocaleTimeString()});
+                        sch_dep_dt: depTime.toLocaleTimeString()
+                    });
                 }
             });
 
             if (foundStop) {
-                $scope.$apply(function () {
-                    $scope.schedules.push({route_name: schedule.route_name,
-                    trip_name: schedule.trip_name,
-                    direction_name: schedule.direction_name,
-                    stops: stops});
+                $scope.$apply(function() {
+                    $scope.schedules.push({
+                        route_name: schedule.route_name,
+                        trip_name: schedule.trip_name,
+                        direction_name: schedule.direction_name,
+                        stops: stops
+                    });
                 });
             }
         });
-    }
+    };
 
 })
 
@@ -103,21 +106,21 @@ angular.module('mbta', [])
 
         navigator.serviceWorker.register('/sw.js').then(function(reg) {
             if (!navigator.serviceWorker.controller) {
-              return;
+                return;
             }
 
             if (reg.waiting) {
-              $scope.updateReady(reg.waiting);
-              return;
+                $scope.updateReady(reg.waiting);
+                return;
             }
 
             if (reg.installing) {
-              $scope.trackInstalling(reg.installing);
-              return;
+                $scope.trackInstalling(reg.installing);
+                return;
             }
 
             reg.addEventListener('updatefound', function() {
-              $scope.trackInstalling(reg.installing);
+                $scope.trackInstalling(reg.installing);
             });
         });
         // Ensure refresh is only called once.
@@ -133,7 +136,7 @@ angular.module('mbta', [])
     $scope.trackInstalling = function(worker) {
         worker.addEventListener('statechange', function() {
             if (worker.state == 'installed') {
-              $scope.updateReady(worker);
+                $scope.updateReady(worker);
             }
         });
     };
@@ -146,9 +149,9 @@ angular.module('mbta', [])
     };
 
     $scope.update = function() {
-        $scope.readyWorker.postMessage({action: 'skipWaiting'});
+        $scope.readyWorker.postMessage({
+            action: 'skipWaiting'
+        });
     };
 
 });
-
-
--- sw.js
+++ sw.js
@@ -3,148 +3,151 @@ import idb from 'idb';
 var staticCacheName = 'mbta-static-v5';
 
 var dbPromise = idb.open('mbta-db', 5, function(upgradeDb) {
-  switch(upgradeDb.oldVersion) {
-    case 0:
-    case 1:
-    case 2:
-    case 3:
-      var tripStore = upgradeDb.createObjectStore('trips', {keyPath: ['tripName','stopName']});
-    case 4:
-      var tripStore1 = upgradeDb.transaction.objectStore('trips');
-      tripStore1.createIndex('stoptime', ['stopName', 'arrival']);
-  }
+    switch (upgradeDb.oldVersion) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+            var tripStore = upgradeDb.createObjectStore('trips', {
+                keyPath: ['tripName', 'stopName']
+            });
+        case 4:
+            var tripStore1 = upgradeDb.transaction.objectStore('trips');
+            tripStore1.createIndex('stoptime', ['stopName', 'arrival']);
+    }
 });
 
 self._processGTFSData = function(gtfsData) {
 
-  dbPromise.then(function(db){
-    var allTextLines = gtfsData.split(/\r\n|\n/);
+    dbPromise.then(function(db) {
+        var allTextLines = gtfsData.split(/\r\n|\n/);
 
-    var tx = db.transaction('trips', 'readwrite');
-    var tripStore = tx.objectStore('trips');
+        var tx = db.transaction('trips', 'readwrite');
+        var tripStore = tx.objectStore('trips');
 
-    allTextLines.forEach(function(line) {
-      var entries = line.split(',');
-      var tripname = entries[0].replace(/['"]+/g, '');
-      //convert to date so we can compare
-      var arr = new Date('January 1, 1970 ' + entries[1].replace(/['"]+/g, ''));
-      var dep = new Date('January 1, 1970 ' + entries[2].replace(/['"]+/g, ''));
-      var stop = entries[3].replace(/['"]+/g, '');
+        allTextLines.forEach(function(line) {
+            var entries = line.split(',');
+            var tripname = entries[0].replace(/['"]+/g, '');
+            //convert to date so we can compare
+            var arr = new Date('January 1, 1970 ' + entries[1].replace(/['"]+/g, ''));
+            var dep = new Date('January 1, 1970 ' + entries[2].replace(/['"]+/g, ''));
+            var stop = entries[3].replace(/['"]+/g, '');
 
-      //add the entry to indexDB trips db
-      tripStore.put({tripName: tripname, arrival: arr, departure: dep, stopName: stop});
+            //add the entry to indexDB trips db
+            tripStore.put({
+                tripName: tripname,
+                arrival: arr,
+                departure: dep,
+                stopName: stop
+            });
+        });
+        return tx.complete;
+    }).then(function() {
+        console.log('added entries to indexDB trips');
+    }).catch(function(error) {
+        console.log(error);
     });
-    return tx.complete;
-  }).then(function() {
-    console.log('added entries to indexDB trips');
-  }).catch(function(error) {
-    console.log(error);
-  });
-}
+};
 
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/all.js',
-        'css/bootstrapcerulean.css',
-        'stop_times_cr.txt'
-        ]);
-    })
-  );
+    event.waitUntil(
+        caches.open(staticCacheName).then(function(cache) {
+            return cache.addAll([
+                '/',
+                'js/all.js',
+                'css/bootstrapcerulean.css',
+                'stop_times_cr.txt'
+            ]);
+        })
+    );
 
-  //get the gtfs -stop_times_cr data1 here, and store all the data to indexdb
-  event.waitUntil(
-    caches.match('stop_times_cr.txt')
-    .then(function(response) {
-      return response.text();
-    }).then(function(text) {
-      return  self._processGTFSData(text);
-    }).catch(function(error) {
-      console.log(error);
-    })
-  );
+    //get the gtfs -stop_times_cr data1 here, and store all the data to indexdb
+    event.waitUntil(
+        caches.match('stop_times_cr.txt')
+        .then(function(response) {
+            return response.text();
+        }).then(function(text) {
+            return self._processGTFSData(text);
+        }).catch(function(error) {
+            console.log(error);
+        })
+    );
 
-  //TODO: addghghg stop.txt, trips.txt, and calendar.txt data.
+    //TODO: addghghg stop.txt, trips.txt, and calendar.txt data.
 
 });
 
 self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('mbta-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('mbta-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
 
-self._getSearchParam = function (searchString, param) {
-  var loc = searchString.indexOf(param+'=')+param.length+1;
-  var nextloc = searchString.indexOf('&', loc);
-  if (nextloc > -1)
-  {
-    return searchString.substr(loc, nextloc-loc);
-  }
-  else
-    return searchString.substr(loc);
+self._getSearchParam = function(searchString, param) {
+    var loc = searchString.indexOf(param + '=') + param.length + 1;
+    var nextloc = searchString.indexOf('&', loc);
+    if (nextloc > -1) {
+        return searchString.substr(loc, nextloc - loc);
+    } else
+        return searchString.substr(loc);
 };
 
 self.addEventListener('fetch', function(event) {
-  var requestUrl = new URL(event.request.url);
-  //console.log('in fetch',requestUrl);
+    var requestUrl = new URL(event.request.url);
+    //console.log('in fetch',requestUrl);
 
-  if (requestUrl.origin === location.origin) {
-    //this is a request for a static resource
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  }
-  else
-  {
-    //this is a schedule request, try to get from indexDB and also realtime
-    if (requestUrl.hostname === 'realtime.mbta.com'
-      && requestUrl.pathname === '/developer/api/v2/schedulebystop' )
-    {
+    if (requestUrl.origin === location.origin) {
+        //this is a request for a static resource
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    } else {
+        //this is a schedule request, try to get from indexDB and also realtime
+        if (requestUrl.hostname === 'realtime.mbta.com' && requestUrl.pathname === '/developer/api/v2/schedulebystop') {
 
-    // get the stop and max_time from requestUrl
-      var stop = self._getSearchParam(requestUrl.search, 'stop');
-      var max_time = self._getSearchParam(requestUrl.search, 'max_time');
+            // get the stop and max_time (in minutes) from requestUrl
+            var stop = self._getSearchParam(requestUrl.search, 'stop');
+            var max_time = self._getSearchParam(requestUrl.search, 'max_time');
 
-      dbPromise.then(function(db) {
-        var tx = db.transaction('trips');
-        var tripStore = tx.objectStore('trips');
-        var stopIndex = tripStore.index('stoptime');
-        var keyRange = IDBKeyRange.bound([stop, new Date('January 1, 1970 00:00:00')],
-          [stop, new Date('January 1, 1970 23:00:00')]);
-        stopIndex.openCursor(keyRange)
-        .then(function logStop(cursor) {
-          if (!cursor)
-            return;
+            var currentDate = new Date();
+            var startTime = new Date('January 1, 1970 ' + currentDate.toLocaleTimeString());
+            var endTime = new Date(startTime.valueOf() + max_time * 60 * 1000);
 
-          console.log(cursor.value.tripName);
+            dbPromise.then(function(db) {
+                var tx = db.transaction('trips');
+                var tripStore = tx.objectStore('trips');
+                var stopIndex = tripStore.index('stoptime');
+                var keyRange = IDBKeyRange.bound([stop, startTime], [stop, endTime]);
+                stopIndex.openCursor(keyRange)
+                    .then(function logStop(cursor) {
+                        if (!cursor)
+                            return;
 
-          return cursor.continue().then(logStop);
-        }).then(function() {
-          console.log('done cursoring');
-        });
-      });
-    //console.log('TODO: get from indexedDB for',requestUrl.search);
+                        console.log(cursor.value.tripName, cursor.value.stopName, cursor.value.arrival);
+
+                        return cursor.continue().then(logStop);
+                    }).then(function() {
+                        console.log('done cursoring');
+                    });
+            });
+        }
     }
-  }
 
 });
 
 self.addEventListener('message', function(event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
 });
